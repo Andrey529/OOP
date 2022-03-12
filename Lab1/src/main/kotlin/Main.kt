14181624e8b5b7aa6a5c1dd6a25f5b245bbb45cd
Andrey <75383288+Andrey529@users.noreply.github.com>
2022-03-13 00:35:13 +0300

    done first lab

--- Lab1/src/main/kotlin/Main.kt
+++ Lab1/src/main/kotlin/Main.kt
@@ -1,7 +1,36 @@
-fun main(args: Array<String>) {
-    println("Hello World!")
+package address
 
-    // Try adding program arguments via Run/Debug configuration.
-    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
-    println("Program arguments: ${args.joinToString()}")
+fun main(args: Array<String>) {
+
+    val adresses = """
+            1. 111111, Москва, ул. Московская проспект проспектный, д. 1
+            2. 22222, Санкт-Петербург, ул. Питерская, д. 2
+            3. 33333, Анапа, ул. Анапская, д. 3
+        """.trimIndent()
+
+    val listAdresses: MutableList<Address>
+    try {
+        listAdresses = parseAddresses(adresses)
+    }
+    catch (e: IllegalArgumentException) {
+        println(e)
+        return
+    }
+
+    for (adress in listAdresses){
+        println(adress)
+    }
+
+    try {
+        println("Adress with the biggest index; index = ${biggestIndex(listAdresses).index}")
+        println("Adress with the smallest index; index = ${smallestIndex(listAdresses).index}")
+
+        println("Adress with the longest street; street = ${addressWithLongestStreet(listAdresses).street}")
+        println("Adress with the shortest street; street = ${addressWithShortestStreet(listAdresses).street}")
+    }
+    catch (e: IllegalArgumentException) {
+        println(e)
+        return
+    }
+
 }
---
+++ Lab1/src/main/kotlin/address/Address.kt
@@ -0,0 +1,106 @@
+package address
+
+fun biggestIndex(addresses: MutableList<Address>) : Address {
+    if (addresses.isEmpty()){
+        throw IllegalArgumentException("Empty list of adresses")
+    }
+    var biggestIndex = addresses[0]
+    for (adress in addresses){
+        if (adress.index > biggestIndex.index){
+            biggestIndex = adress
+        }
+    }
+    return biggestIndex
+}
+
+fun smallestIndex(addresses: MutableList<Address>) : Address {
+    if (addresses.isEmpty()){
+        throw IllegalArgumentException("Empty list of adresses")
+    }
+    var smallestIndex = addresses[0]
+    for (adress in addresses){
+        if (adress.index < smallestIndex.index){
+            smallestIndex = adress
+        }
+    }
+    return smallestIndex
+}
+
+fun addressWithLongestStreet(addresses: MutableList<Address>) : Address {
+    if (addresses.isEmpty()){
+        throw IllegalArgumentException("Empty list of adresses")
+    }
+    var biggestStreet = addresses[0]
+    for (adress in addresses){
+        if (adress.street.length > biggestStreet.street.length) {
+            biggestStreet = adress
+        }
+    }
+    return biggestStreet
+}
+
+fun addressWithShortestStreet(addresses: MutableList<Address>) : Address {
+    if (addresses.isEmpty()){
+        throw IllegalArgumentException("Empty list of adresses")
+    }
+    var smallestStreet = addresses[0]
+    for (adress in addresses){
+        if (adress.street.length < smallestStreet.street.length) {
+            smallestStreet = adress
+        }
+    }
+    return smallestStreet
+}
+
+
+fun parseAddresses(addresses: String) : MutableList<Address> {
+    if (addresses.isEmpty()){
+        throw IllegalArgumentException("Empty string")
+    }
+
+    val listAddresses = mutableListOf<Address>()
+    val strings = addresses.split('\n')
+
+    for (str in strings) {
+        val partString = str.split(',') // [N. ИндексN, Название города N, ул. Название улицы, д. Номер дома]
+
+        val indexPart = partString[0].split('.') // [N, ИндексN]
+        val index = indexPart[1].drop(1).toInt() // ИндексN
+
+        val city = partString[1].drop(1)  // Название города N
+        val street = partString[2].drop(5) // Название улицы
+        val buildingNumber = partString[3].drop(4).toInt() // Номер дома
+
+        val address = Address(index, city, street, buildingNumber)
+        listAddresses.add(address)
+    }
+    return listAddresses
+}
+
+data class Address(val index: Int, val city: String, val street: String, val building: Int) {
+    override fun toString(): String {
+        return "Address: index = $index, city = $city, street = $street, building number = $building"
+    }
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as Address
+
+        if (index != other.index) return false
+        if (city != other.city) return false
+        if (street != other.street) return false
+        if (building != other.building) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        var result = index
+        result = 31 * result + city.hashCode()
+        result = 31 * result + street.hashCode()
+        result = 31 * result + building
+        return result
+    }
+}
