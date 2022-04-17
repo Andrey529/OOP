87f021ab3d802b639a0533688ac54ced38bc69dd
Andrey529 <andr02eyes@gmail.com>
2022-04-17 20:02:35 +0300

    done contact for people

---
+++ src/main/kotlin/lab3/contact/Contact.kt
@@ -0,0 +1,8 @@
+package lab3.contact
+
+sealed interface Contact {
+    class Phone
+    class Email
+    class Address
+    class LinkToSocialNetwork
+}
---
+++ src/main/kotlin/lab3/contact/PhoneType.kt
@@ -0,0 +1,7 @@
+package lab3.contact
+
+enum class PhoneType {
+    MOBILE,
+    HOME,
+    WORKING
+}
