2b44ba6d35e9fc0a551b02a063e32f72f61b925c
Andrey529 <andr02eyes@gmail.com>
2022-04-28 00:19:35 +0300

    done move function

--- src/main/kotlin/lab4/Main.kt
+++ src/main/kotlin/lab4/Main.kt
@@ -1,12 +1,34 @@
 package lab4
 
 import lab4.model.Model
+import lab4.model.Move
 import java.io.File
 
+/*
+    ########
+    ###----#
+    S-#-##-#
+    #---##-F
+    ########
+*/
 
 fun main(args: Array<String>) {
-    val model = Model()
 
+    val model = Model()
     model.printBoard()
+
+    model.doMove(Move.RIGHT)
+    model.doMove(Move.DOWN)
+    model.doMove(Move.RIGHT)
+    model.doMove(Move.RIGHT)
+    model.doMove(Move.UP)
+    model.doMove(Move.UP)
+    model.doMove(Move.RIGHT)
+    model.doMove(Move.RIGHT)
+    model.doMove(Move.RIGHT)
+    model.doMove(Move.DOWN)
+    model.doMove(Move.DOWN)
+    model.doMove(Move.RIGHT)
+//    model.doMove(Move.RIGHT)
 
 }
--- src/main/kotlin/lab4/model/Model.kt
+++ src/main/kotlin/lab4/model/Model.kt
@@ -14,12 +14,63 @@ enum class Cell(private val textValue: String) {
 
 enum class State(val textValue: String) {
     WIN("Labyrinth finished. Congrats!!!"),
-    NOT_YET_WIN("Waiting for the move")
+    NOT_YET_WIN("Waiting for the move");
+
+    override fun toString(): String = textValue
+}
+
+data class Position(var x: Int, var y: Int)
+
+enum class Move(val textValue: String) {
+    UP("Up"),
+    DOWN("Down"),
+    RIGHT("Right"),
+    LEFT("Left");
+
+    override fun toString(): String = textValue
 }
 
 
 class Model(fileWithLabyrinth: File = File("src/main/resources/test.txt")) {
-    private val _board = readBoardFromFile(fileWithLabyrinth)
+    private val _board: MutableMap<Pair<Int, Int>, Char> = readBoardFromFile(fileWithLabyrinth)
+    val board: MutableMap<Pair<Int, Int>, Char>
+        get() = _board
+
+    private val _startPosition = Position(
+        _board.filter { it.value == 'S' }.keys.first().second,
+        _board.filter { it.value == 'S' }.keys.first().first
+    )
+
+    private val _finishPosition = Position(
+        _board.filter { it.value == 'F' }.keys.first().second,
+        _board.filter { it.value == 'F' }.keys.first().first
+    )
+
+    private var _currentPosition = _startPosition
+
+    var state: State = State.NOT_YET_WIN
+    private val _boardHeight = _board.keys.maxOf { it.first }
+    private val _boardWidth = _board.keys.maxOf { it.second }
+
+    fun doMove(move: Move) {
+        require(state == State.NOT_YET_WIN) { "Labyrinth finished" }
+        require((move == Move.LEFT && _currentPosition.x != 0) ||
+                (move == Move.RIGHT && _currentPosition.x != _boardWidth) ||
+                (move == Move.UP && _currentPosition.y != 0) ||
+                (move == Move.DOWN && _currentPosition.y != _boardHeight)) { "It is impossible to make such a move" }
+
+        // update board
+        when(move) {
+            Move.LEFT -> --_currentPosition.x
+            Move.RIGHT -> ++_currentPosition.x
+            Move.UP -> --_currentPosition.y
+            Move.DOWN -> ++_currentPosition.y
+        }
+
+        // check win
+        state = if (_currentPosition == _finishPosition) State.WIN else State.NOT_YET_WIN
+    }
+
 
     fun readBoardFromFile(file: File): MutableMap<Pair<Int, Int>, Char> {
         val board = mutableMapOf<Pair<Int, Int>, Char>()
@@ -38,5 +89,6 @@ class Model(fileWithLabyrinth: File = File("src/main/resources/test.txt")) {
             if (it.key.second == 0 && it.key.first != 0) println()
             print(it.value)
         }
+        println()
     }
 }
