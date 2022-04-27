2895e011b5863a8599bda305654807b204b6f826
Andrey529 <andr02eyes@gmail.com>
2022-04-27 20:27:50 +0300

    done a parsing of the labyrinth from a file

--- src/main/kotlin/lab4/Main.kt
+++ src/main/kotlin/lab4/Main.kt
@@ -1,2 +1,12 @@
 package lab4
 
+import lab4.model.Model
+import java.io.File
+
+
+fun main(args: Array<String>) {
+    val model = Model()
+
+    model.printBoard()
+
+}
--- src/main/kotlin/lab4/model/Model.kt
+++ src/main/kotlin/lab4/model/Model.kt
@@ -1,5 +1,8 @@
 package lab4.model
 
+import java.io.File
+
+
 enum class Cell(private val textValue: String) {
     WALL("#"),
     WAY("-"),
@@ -14,6 +17,26 @@ enum class State(val textValue: String) {
     NOT_YET_WIN("Waiting for the move")
 }
 
-class Model {
-//    private val _board: MutableMap =
+
+class Model(fileWithLabyrinth: File = File("src/main/resources/test.txt")) {
+    private val _board = readBoardFromFile(fileWithLabyrinth)
+
+    fun readBoardFromFile(file: File): MutableMap<Pair<Int, Int>, Char> {
+        val board = mutableMapOf<Pair<Int, Int>, Char>()
+        file.useLines { lines ->
+            lines.withIndex().forEach { line ->
+                line.value.withIndex().forEach { sumbols ->
+                    board.put(Pair(line.index, sumbols.index), sumbols.value)
+                }
+            }
+        }
+        return board
+    }
+
+    fun printBoard() {
+        _board.forEach {
+            if (it.key.second == 0 && it.key.first != 0) println()
+            print(it.value)
+        }
+    }
 }
